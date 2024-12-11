e778c17236bcd3c73e25c6fe97601347c42968c1
Mudassir Chapra <muddi900@gmail.com>
2024-12-11 14:26:42 +0500

    Logging  output

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
-import { GoogleSpreadsheet } from "npm:google-spreadsheet";
-import { JWT } from "npm:google-auth-library";
+import { getSpreadsheet } from "./sheet.ts";
 
-export async function getJwt(filePath?: string): Promise<JWT> {
-  const jsonText = await Deno.readTextFile(filePath || "./client_secret.json");
+const ss = await getSpreadsheet();
 
-  return new JWT(...JSON.parse(jsonText));
-}
+await ss.loadInfo();
+
+const ws = ss.sheetsByIndex[0];
+await ws.loadHeaderRow();
+const headers = ws.headerValues;
+const rows = [
+  headers,
+  ...(await ws.getRows()).map((row, _) => Object.values(row.toObject())),
+];
+console.table(rows);
+// ┌───────┬──────────────┬─────────────┬──────────────────────┬──────┐
+// │ (idx) │ 0            │ 1           │ 2                    │ 3    │
+// ├───────┼──────────────┼─────────────┼──────────────────────┼──────┤
+// │     0 │ "First Name" │ "Last Name" │ "Email"              │ "ID" │
+// │     1 │ "Alif"       │ "Bay"       │ "alif.bay@fake.fake" │ "1"  │
+// │     2 │ "Eh"         │ "Bee"       │ "a.b@fake.fake"      │ "2"  │
+// │     3 │ "Ecks"       │ "Wye"       │ "e.w@fake.fake"      │ "3"  │
+// └───────┴──────────────┴─────────────┴──────────────────────┴──────┘
