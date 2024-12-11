29efa4223613eb728d7be95a9cdd33466f10ae04
Mudassir Chapra <muddi900@gmail.com>
2024-12-11 13:14:20 +0500

    fix the formating

--- sheet.ts
+++ sheet.ts
@@ -1,16 +1,21 @@
 import { GoogleSpreadsheet } from "npm:google-spreadsheet";
 import { JWT } from "npm:google-auth-library";
 
-export async function getJwt(filePath?: string): Promise<JWT> {
+export async function getAuth(filePath?: string): Promise<JWT> {
   const jsonText = await Deno.readTextFile(filePath || "./client_secret.json");
 
-  return new JWT(...JSON.parse(jsonText));
+  const clientSecret = JSON.parse(jsonText);
+  return new JWT({
+    email: clientSecret.client_email,
+    key: clientSecret.private_key,
+    scopes: clientSecret.scopes,
+  })
 }
 
 export async function getSpreadsheet(
   sheetId?: string
-): Promise<GoogleSpreadsheet> {
-  const auth = await getJwt();
+): Promise<GoogleSpreadsheet>{
+  const auth = await getAuth()
 
   return new GoogleSpreadsheet(
     sheetId || Deno.env.get("SHEET_ID")!,
