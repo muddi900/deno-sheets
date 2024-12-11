1780760d01fc900993dc7e63ef90a76d165b8449
Mudassir Chapra <muddi900@gmail.com>
2024-12-11 12:00:56 +0500

    Adding imports for Sheet and Auth

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { GoogleSpreadsheet } from "npm:google-spreadsheet";
+import { JWT } from "npm:google-auth-library";
+
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
